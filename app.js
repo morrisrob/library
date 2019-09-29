6749d91b3897783c1f76709eb2097a09ccd4ea1f
morrisrobert <morrisrobert@outlook.com>
2019-09-29 18:27:17 -0500

    Code cleaned up
    code cleaned up

--- app.js
+++ app.js
@@ -33,65 +33,55 @@ function displayBooks() {
     clearTable();
     buildHeader();
 
-
     myLibrary.forEach(function (element, i) {
-        var row = table.insertRow(-1);
-        var cell = row.insertCell();
-        // cell.innerHTML = myLibrary[i].recordNum;
+        let propertyNames = (Object.getOwnPropertyNames(myLibrary[0]));
+        let columnCount = 4;
+        let row = table.insertRow(-1);
+
+        let cell = row.insertCell();
+        cell.innerHTML = myLibrary[i].recordNum;
         cell.innerHTML = i;
-        var cell = row.insertCell();
-        cell.innerHTML = myLibrary[i].title
-        var cell = row.insertCell();
-        cell.innerHTML = myLibrary[i].author
-        var cell = row.insertCell();
-        cell.innerHTML = myLibrary[i].pages
-        var cell = row.insertCell();
-        cell.innerHTML = myLibrary[i].read
-        
-        var cell = row.insertCell();
-        var btnDel = document.createElement("BUTTON");   // Create a <button> element
+
+        for (j = 0; j < columnCount; j++) {
+            let cell = row.insertCell(-1);
+            cell.innerHTML = myLibrary[i][propertyNames[j]];
+        }
+
+        cell = row.insertCell();
+        let btnDel = document.createElement("BUTTON"); 
         btnDel.innerHTML = "Delete Book";
         let btnDelID = 'delBtn ' + i;
         btnDel.id = btnDelID;
         btnDel.classList.add('delBtn');
         cell.appendChild(btnDel);
 
-        var cell = row.insertCell();
-        var btnReadStatus = document.createElement("BUTTON");   // Create a <button> element
-        
+        cell = row.insertCell();
+        let btnReadStatus = document.createElement("BUTTON");
+
         if(myLibrary[i].read === 'yes') {
             btnReadStatus.innerHTML = "Change to Unread";
         } else {
             btnReadStatus.innerHTML = "Change to Read";
         }
-        
+
         let btnReadStatusID = 'btnReadStatus ' + i;
         btnReadStatus.id = btnReadStatusID;
         btnReadStatus.classList.add('btnReadStatus');
         cell.appendChild(btnReadStatus);
 
-
-
-        // console.log(recordNum2);
-
         document.getElementById(btnDelID).addEventListener("click", function () {
             deleteBook(i);
-            console.log('delete button pressed for btnId ' + btnDelID);
         })
 
         document.getElementById(btnReadStatusID).addEventListener("click", function () {
             changeReadStatus(i);
-            console.log('change status button pressed for btnDelID ' + btnDelID);
         })
     })
 }
 
 function deleteBook(num) {
     myLibrary.splice(num, 1);
-    // var table = document.getElementById("libraryTable");
-    // table.refresh();
     displayBooks();
-    console.log('deleted num ' + num);
 }
 
 function recordCounter() {
@@ -100,7 +90,6 @@ function recordCounter() {
 
 function addBook() {
     let readValue;
-    // let recordNum = myLibrary.length;
     let titleValue = document.getElementById('title').value;
     let authorValue = document.getElementById('author').value;
     let pagesValue = document.getElementById('pages').value;
@@ -109,12 +98,8 @@ function addBook() {
     } else if(document.getElementById('readNo').checked == true) {
         readValue = 'no';
     }
-
     let newBook = new Book(titleValue, authorValue, pagesValue, readValue);
-    myLibrary.push(newBook);
-
-    console.log(myLibrary);
-    
+    myLibrary.push(newBook);    
     displayBooks();
     clearTextBoxes();
     return false;
